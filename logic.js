117e2e1bbb5e941c103569c20850da9d4d5919fc
gh0stl0nelyGit <khoinguyen19971997@gmail.com>
2020-03-07 11:03:50 -0500

    ok

--- logic.js
+++ logic.js
@@ -208,17 +208,3 @@ var data = {
     el.innerHTML = name;
     return el;
   }
-
-  // puliing categories from index 
-
-  
-  for (var i = 0; i < data.categories.length; i++) {
-    var category = data.categories[i];
-    var categoryElement = createCategoryElement(category.name, category.url_title);
-    container.appendChild(categoryElement);
-  
-    
-    if (i + 1 < data.categories.length) {
-      container.appendChild(salary);
-    }
-  }
