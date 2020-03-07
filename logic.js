602049871a7d9e6bb9c9be929311dba3c3905ea0
Demi Wu <demiwu96@gmail.com>
2020-03-07 13:14:38 -0500

    enter your category function

--- logic.js
+++ logic.js
@@ -65,8 +65,8 @@ function proposeBudget(event) {
     // grab the value of salary
     var salaryCal = salary.value
     var savingCal = saving.value
-    var calculation = ((salaryCal * savingCal)/100)
-    alert(calculation)
+    var calculation = ((salaryCal * savingCal) / 100)
+    M.toast({ html: calculation, classes: 'red' });
     // grab the value of saving
     // do your calculation
     // alert calculation
@@ -93,96 +93,110 @@ function proposeBudget(event) {
     if (saving.value > 100 || saving.value < 0) {
         saving.style.backgroundColor = "#ff000042";
         saving.style.color = "red";
-        alert("Please enter a number from 1 to 100.");
+        M.toast({ html: 'Please enter a number from 0 to 100.', classes: 'red' });
     };
 
     // choose at least one category
     var chips = document.getElementsByClassName("chip");
     if (chips.length == 0 || !chips) {
-        alert("Please choose at least one category.")
+        M.toast({ html: "Please choose at least one category.", classes: 'red' });
     };
 };
 
 var inflation;
 var countrySource;
-function getCountry(event){
+function getCountry(event) {
     event.preventDefault();
     fetch('https://extreme-ip-lookup.com/json/')
-    .then( res => res.json())
-    .then(response => {
-        countrySource = response.country.toLowerCase(); 
-        
-    })
-    .catch((data, status) => {
-        console.log('Get Country Request failed');
-    });
+        .then(res => res.json())
+        .then(response => {
+            countrySource = response.country.toLowerCase();
+
+        })
+        .catch((data, status) => {
+            console.log('Get Country Request failed');
+        });
 }
 
 function getInflation(event) {
-event.preventDefault();
-// get current date
-var fullDate = new Date();
-var twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1); 
-var currentDate = fullDate.getFullYear() + "/" + twoDigitMonth + "/" + fullDate.getDate();
+    event.preventDefault();
+    // get current date
+    var fullDate = new Date();
+    var twoDigitMonth = ((fullDate.getMonth().length + 1) === 1) ? (fullDate.getMonth() + 1) : '0' + (fullDate.getMonth() + 1);
+    var currentDate = fullDate.getFullYear() + "/" + twoDigitMonth + "/" + fullDate.getDate();
 
-// get date from 1 year ago for range of inflation calculation
-var prevDate = (fullDate.getFullYear() - 4)+ "/" + twoDigitMonth + "/" + fullDate.getDate();
+    // get date from 1 year ago for range of inflation calculation
+    var prevDate = (fullDate.getFullYear() - 4) + "/" + twoDigitMonth + "/" + fullDate.getDate();
 
-var apiUrl = 'https://www.statbureau.org/calculate-inflation-price-jsonp?jsoncallback=?';
+    var apiUrl = 'https://www.statbureau.org/calculate-inflation-price-jsonp?jsoncallback=?';
 
-//default country to canada if one is not found
-if (countrySource === null) {
-    countrySource = 'canada';
-}
+    //default country to canada if one is not found
+    if (countrySource === null) {
+        countrySource = 'canada';
+    }
 
-function calculate() {
-  $.getJSON(apiUrl, {
-      country: countrySource,
-      start: prevDate,
-      end: currentDate,
-      amount: 100,
-      format: true
-    })
-    .done(function (data) {        
-        var temp_val = data.replace("$", "");
-        inflation = (Number(temp_val) - 100) / 5 ;
-    });
+    function calculate() {
+        $.getJSON(apiUrl, {
+            country: countrySource,
+            start: prevDate,
+            end: currentDate,
+            amount: 100,
+            format: true
+        })
+            .done(function (data) {
+                var temp_val = data.replace("$", "");
+                inflation = (Number(temp_val) - 100) / 5;
+            });
     }
 };
 
 var data = {
     categories: [
-      {name: 'one', url_title: 'oneUrl'},
-      {name: 'two', url_title: 'twoUrl'}
+        { name: 'one', url_title: 'oneUrl' },
+        { name: 'two', url_title: 'twoUrl' }
     ],
-  };
-  
-  var container = document.getElementById('container');
-  var comma = document.createTextNode(', ');
-  
-  function createCategoryElement(name, url) {
+};
+
+var container = document.getElementById('container');
+var comma = document.createTextNode(', ');
+
+function createCategoryElement(name, url) {
     var urlBase = '#journal-category-';
     var cssClass = 'js-page-link';
-  
+
     var el = document.createElement('a');
     el.setAttribute('href', urlBase + url);
     el.setAttribute('class', cssClass);
     el.innerHTML = name;
     return el;
-  }
-  
-  // puliing categories from index 
-  function appendToBudget(){
-  for (var i = 0; i < data.categories.length; i++) {
-    var category = data.categories[i];
-    var categoryElement = createCategoryElement(category.name, category.url_title);
-    container.appendChild(categoryElement);
-  
-    
-    if (i + 1 < data.categories.length) {
-      container.appendChild(salary);
-    }
-  }
 }
-          
-          
+
+// puliing categories from index 
+function appendToBudget() {
+    for (var i = 0; i < data.categories.length; i++) {
+        var category = data.categories[i];
+        var categoryElement = createCategoryElement(category.name, category.url_title);
+        container.appendChild(categoryElement);
+
+
+        if (i + 1 < data.categories.length) {
+            container.appendChild(salary);
+        }
+    }
+}
+// modal
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+});
+
+var addInputBttn = document.getElementById("addInputBttn");
+addInputBttn.addEventListener("click", addInputField);
+function addInputField(){
+    var inputDiv = document.getElementById("inputDiv");
+    var inputField = document.createElement("input");
+    inputField.setAttribute("type", "text");
+    inputField.setAttribute("id", "customCategory");
+    inputField.classList.add("validate");
+    inputDiv.appendChild(inputField);
+}
