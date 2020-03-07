d536e9659edd5262014b823363bb94905cc171b1
gh0stl0nelyGit <khoinguyen19971997@gmail.com>
2020-03-07 10:45:45 -0500

    Enhanced budget functionality

--- logic.js
+++ logic.js
@@ -1,35 +1,35 @@
-// displayTagsFromStorage(); Remember to de-comment!! 
+// displayTagsFromStorage(); Remember to de-comment!!
 
 function displayTagsFromStorage() {
-    var storage = JSON.parse(localStorage.getItem('chips')); // []
+  var storage = JSON.parse(localStorage.getItem("chips")); // []
 
-    if (storage) {
-        var budgetOptions = document.getElementById('budget-options');
+  if (storage) {
+    var budgetOptions = document.getElementById("budget-options");
 
-        for (var i = 0; i < storage.length; i++) {
-            createAndDisplayTag(storage, i, budgetOptions);
-        }
-    } else {
-        storage = [];
-        localStorage.setItem('chips', JSON.stringify(storage));
+    for (var i = 0; i < storage.length; i++) {
+      createAndDisplayTag(storage, i, budgetOptions);
     }
+  } else {
+    storage = [];
+    localStorage.setItem("chips", JSON.stringify(storage));
+  }
 }
 
 //Helper for displayTagsFromStorage
 function createAndDisplayTag(storage, i, budgetOptions) {
-    var divItem = document.createElement('div');
-    divItem.classList.add('chip');
-    divItem.setAttribute('data-name', storage[i]);
-    divItem.innerHTML = storage[i];
+  var divItem = document.createElement("div");
+  divItem.classList.add("chip");
+  divItem.setAttribute("data-name", storage[i]);
+  divItem.innerHTML = storage[i];
 
-    var icon = document.createElement('i');
-    icon.classList.add('close');
-    icon.classList.add('material-icons');
-    icon.innerText = 'close';
+  var icon = document.createElement("i");
+  icon.classList.add("close");
+  icon.classList.add("material-icons");
+  icon.innerText = "close";
 
-    divItem.appendChild(icon);
-    console.log(divItem)
-    budgetOptions.appendChild(divItem);
+  divItem.appendChild(icon);
+  console.log(divItem);
+  budgetOptions.appendChild(divItem);
 }
 
 // * Whenver the user about to leave the tab, the browser will save all the current chip inside local storage
@@ -55,132 +55,135 @@ function createAndDisplayTag(storage, i, budgetOptions) {
 var submit = document.getElementById("submit-button");
 submit.addEventListener("click", proposeBudget);
 
-
 function proposeBudget(event) {
-    event.preventDefault();
-    // check for valid income and saving percentage input
-    var salary = document.getElementById("salary");
-    var saving = document.getElementById("saving");
+  event.preventDefault();
+  // check for valid income and saving percentage input
+  var salary = document.getElementById("salary");
+  var saving = document.getElementById("saving");
 
-    // grab the value of salary
-    var salaryCal = salary.value
-    var savingCal = saving.value
-    var calculation = ((salaryCal * savingCal)/100)
-    alert(calculation)
-    // grab the value of saving
-    // do your calculation
-    // alert calculation
+  // grab the value of salary
+  var salaryCal = salary.value;
+  var savingCal = saving.value;
+  var calculation = (salaryCal * savingCal) / 100;
+  alert(calculation);
+  // grab the value of saving
+  // do your calculation
+  // alert calculation
 
-    var regex = /\d*\.?\d*$/;
-    // if the input is not vaild, the input box will turn red
-    if (!regex.test(salary.value)) {
-        salary.style.backgroundColor = "#ff000042";
-        salary.style.color = "red";
-    } else {
-        salary.style.backgroundColor = "none";
-        salary.style.color = "black";
-    };
+  var regex = /\d*\.?\d*$/;
+  // if the input is not vaild, the input box will turn red
+  if (!regex.test(salary.value)) {
+    salary.style.backgroundColor = "#ff000042";
+    salary.style.color = "red";
+  } else {
+    salary.style.backgroundColor = "none";
+    salary.style.color = "black";
+  }
 
-    if (!regex.test(saving.value)) {
-        saving.style.backgroundColor = "#ff000042";
-        saving.style.color = "red";
-    } else {
-        saving.style.backgroundColor = "white";
-        saving.style.color = "black";
-    };
+  if (!regex.test(saving.value)) {
+    saving.style.backgroundColor = "#ff000042";
+    saving.style.color = "red";
+  } else {
+    saving.style.backgroundColor = "white";
+    saving.style.color = "black";
+  }
 
-    // user need to enter a number between 0-100 for percentage
-    if (saving.value > 100 || saving.value < 0) {
-        saving.style.backgroundColor = "#ff000042";
-        saving.style.color = "red";
-        alert("Please enter a number from 1 to 100.");
-    };
+  // user need to enter a number between 0-100 for percentage
+  if (saving.value > 100 || saving.value < 0) {
+    saving.style.backgroundColor = "#ff000042";
+    saving.style.color = "red";
+    alert("Please enter a number from 1 to 100.");
+  }
 
-    // choose at least one category
-    var chips = document.getElementsByClassName("chip");
-    if (chips.length == 0 || !chips) {
-        alert("Please choose at least one category.")
-    };
-};
+  // choose at least one category
+  var chips = document.getElementsByClassName("chip");
+  if (chips.length == 0 || !chips) {
+    alert("Please choose at least one category.");
+  }
+}
 
 var inflation;
 var countrySource;
-function getCountry(event){
-    event.preventDefault();
-    fetch('https://extreme-ip-lookup.com/json/')
-    .then( res => res.json())
+
+function getCountry(event) {
+  event.preventDefault();
+  fetch("https://extreme-ip-lookup.com/json/")
+    .then(res => res.json())
     .then(response => {
-        countrySource = response.country.toLowerCase(); 
-        
+      countrySource = response.country.toLowerCase();
     })
     .catch((data, status) => {
-        console.log('Get Country Request failed');
+      console.log("Get Country Request failed");
     });
 }
 
 function getInflation(event) {
-event.preventDefault();
-// get current date
-var fullDate = new Date();
-var twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1); 
-var currentDate = fullDate.getFullYear() + "/" + twoDigitMonth + "/" + fullDate.getDate();
+  event.preventDefault();
+  // get current date
+  var fullDate = new Date();
+  var twoDigitMonth =
+    fullDate.getMonth().length + 1 === 1
+      ? fullDate.getMonth() + 1
+      : "0" + (fullDate.getMonth() + 1);
+  var currentDate =
+    fullDate.getFullYear() + "/" + twoDigitMonth + "/" + fullDate.getDate();
 
-// get date from 1 year ago for range of inflation calculation
-var prevDate = (fullDate.getFullYear() - 4)+ "/" + twoDigitMonth + "/" + fullDate.getDate();
+  // get date from 1 year ago for range of inflation calculation
+  var prevDate =
+    fullDate.getFullYear() - 4 + "/" + twoDigitMonth + "/" + fullDate.getDate();
 
-var apiUrl = 'https://www.statbureau.org/calculate-inflation-price-jsonp?jsoncallback=?';
+  var apiUrl =
+    "https://www.statbureau.org/calculate-inflation-price-jsonp?jsoncallback=?";
 
-//default country to canada if one is not found
-if (countrySource === null) {
-    countrySource = 'canada';
-}
+  //default country to canada if one is not found
+  if (countrySource === null) {
+    countrySource = "canada";
+  }
 
-function calculate() {
-  $.getJSON(apiUrl, {
+  function calculate() {
+    $.getJSON(apiUrl, {
       country: countrySource,
       start: prevDate,
       end: currentDate,
       amount: 100,
       format: true
-    })
-    .done(function (data) {        
-        var temp_val = data.replace("$", "");
-        inflation = (Number(temp_val) - 100) / 5 ;
+    }).done(function(data) {
+      var temp_val = data.replace("$", "");
+      inflation = (Number(temp_val) - 100) / 5;
     });
-    }
-};
-
-var data = {
-    categories: [
-      {name: 'one', url_title: 'oneUrl'},
-      {name: 'two', url_title: 'twoUrl'}
-    ],
-  };
-  
-  var container = document.getElementById('container');
-  var comma = document.createTextNode(', ');
-
-function createCategoryElement(name, url) {
-    var urlBase = '#journal-category-';
-    var cssClass = 'js-page-link';
-  
-    var el = document.createElement('a');
-    el.setAttribute('href', urlBase + url);
-    el.setAttribute('class', cssClass);
-    el.innerHTML = name;
-    return el;
+  }
 }
 
-  for (var i = 0; i < data.categories.length; i++) {
-    var category = data.categories[i];
-    var categoryElement = createCategoryElement(category.name, category.url_title);
-    container.appendChild(categoryElement);
-  
-    
-    if (i + 1 < data.categories.length) {
-      container.appendChild(comma);
-    }
+var data = {
+  categories: [
+    { name: "one", url_title: "oneUrl" },
+    { name: "two", url_title: "twoUrl" }
+  ]
+};
+
+var container = document.getElementById("container");
+var comma = document.createTextNode(", ");
+
+function createCategoryElement(name, url) {
+  var urlBase = "#journal-category-";
+  var cssClass = "js-page-link";
+
+  var el = document.createElement("a");
+  el.setAttribute("href", urlBase + url);
+  el.setAttribute("class", cssClass);
+  el.innerHTML = name;
+  return el;
+}
+
+for (var i = 0; i < data.categories.length; i++) {
+  var category = data.categories[i];
+  var categoryElement = createCategoryElement(
+    category.name,
+    category.url_title
+  );
+  container.appendChild(categoryElement);
+
+  if (i + 1 < data.categories.length) {
+    container.appendChild(comma);
   }
-  
-          
-          
+}
