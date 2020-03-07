e141d58fe2d32f678fb3d5e518817b5a31cbde91
Ibrahem Rian <ryibrahem1210@gmail.com>
2020-03-07 12:34:27 -0500

    percentage calculation functions

--- logic.js
+++ logic.js
@@ -147,7 +147,7 @@ var data = {
     ],
   };
   
-  var container = document.getElementById('container');
+  var container = document.getElementsByClassName('chips');
   var comma = document.createTextNode(', ');
   
   function createCategoryElement(name, url) {
